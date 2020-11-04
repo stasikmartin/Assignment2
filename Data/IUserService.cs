7ed33927a410605c26fba9764c001a05b1236620
stasikmartin <58514490+stasikmartin@users.noreply.github.com>
2020-11-04 18:22:56 +0100

    Add files via upload

---
+++ Data/IUserService.cs
@@ -0,0 +1,7 @@
+﻿using LoginExample.Models;
+
+namespace LoginExample.Data {
+    public interface IUserService {
+        User ValidateUser(string userName, string password);
+    }
+}
---
+++ Data/Impl/PersonsImpl2.0.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LoginExample.Data.Impl
+{
+    public class PersonsImpl2_0 : IPersons
+    {
+        public async Task<List<Adult>> GetPersons()
+        {
+            HttpClient client = new HttpClient();
+            string message = await client.GetStringAsync("http://dnp.metamate.me/Adults");
+            List<Adult> result = JsonSerializer.Deserialize<List<Adult>>(message);
+            return result;
+            
+        }
+
+        public async Task AddPerson(Adult person)
+        {
+            HttpClient client = new HttpClient();
+            string toPost = JsonSerializer.Serialize(person);
+            Console.WriteLine(toPost);
+            StringContent content = new StringContent(toPost, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PutAsync("http://dnp.metamate.me/Adults", content);
+            //HttpResponseMessage response = await client.PostAsync("https://localhost:5003/Todos",content);
+            Console.WriteLine(response.StatusCode);
+        }
+
+        public async Task RemovePerson(int id)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.DeleteAsync("http://dnp.metamate.me/Adults/"+id);
+            Console.WriteLine(response.StatusCode);
+        }
+    }
+}
